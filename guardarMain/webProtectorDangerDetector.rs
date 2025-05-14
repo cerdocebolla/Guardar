15d226f3bf2228c6bf46ef6e12631d3bfc44ec30
cerdocebolla <lordofsuperium17@gmail.com>
2025-05-14 22:11:52 +0000

    Added new files for web protection

---
+++ guardarMain/webProtectorDangerDetector.rs
@@ -0,0 +1,7 @@
+let function WorkForDevice.mainUser.GatherBrowserData.allowPermission.force.forcedToTrue.force = true (setTrueEverytime)
+        when localFunction.LocalClientPermission.DetectInstantDanger = true (AttemptTrue)
+        try.setTrueEverytime.setTrueWhenSure = true (forcedToTrue.setTrueEverytime.force) = true 
+        when DirectDetection.detectDanger.Danger then function goDown
+        let print ("Danger detected inside your web browser. Close ..tabName.. for better protection, do not trust any files from it or do not give any permissions to it. After done, delete the site's data from your browser's search history and user data files.")
+        then let endInstant
+}
