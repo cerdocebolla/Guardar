d79f8e13d3bcb353a7577fd8360eaf0fed62b258
cerdocebolla <lordofsuperium17@gmail.com>
2025-05-15 10:40:21 +0000

    dataGatherer File Added

---
+++ guardarMain/dataGatherer.lua
@@ -0,0 +1,19 @@
+local function = askForData.setTrue = try.setTrue
+    when function = let setTrue then askForpermission.MainDevice.mainUser.force = true (setTrueEverytime)
+        then let print 
+            ("Guardar needs your permission to activate any of the services.")
+            then contuniue 
+                as print 
+                ("Allow Guardar to gather data that is needed?")
+                when mainUser.setAnswer.force = true 
+                then let endInstant.ByPass = true 
+                    then endInstant.end 
+                }
+            }
+        }
+    }
+}
+else let function closeSoftware.shutSoftwareDown = whenSure.setSolution
+    as false 
+    then endInstant.end)
+}
