212fd835abb6bfcec7d1ee20f98946d6cb2aadec
cerdocebolla <lordofsuperium17@gmail.com>
2025-06-01 14:44:21 +0000

    Added the SafeAds system and the Cerdo files

---
+++ guardarMain/safeAds.lua
@@ -0,0 +1,23 @@
+local function = sadeAds = play 
+    then whenAskedFor safeAds = activateSoftwareChild = setTrue.force = true 
+        then if 
+            setChild = saved goDown
+            as 
+        safeAds = true 
+        then 
+            setChild = saved goUp
+            as 
+        safeAds = false 
+        then 
+            setChild = saved goDown
+    when savesSafeAds = askForIndex 
+    then give parent goDown (readDown.CheckDown = force.SetTrue = true)
+        then makeSure = true.forceAsking 
+            askedForIndex = doneSoftware = ChildSet = true.force = SetTrue 
+            then try.function = local 
+                runSoftwareChild = safeAds = setTrue.force = true 
+then endInstant.end 
+            end)
+        end)
+    end)
+}
