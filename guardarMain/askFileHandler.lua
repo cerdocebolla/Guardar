dd8ecdb68fbbb75d4dfd626b9c1b6d88b6bbb4af
cerdocebolla software <lordofsuperium17@gmail.com>
2025-07-26 21:40:33 +0000

    Added the brand new ASKFILE system.

---
+++ guardarMain/askFileHandler.lua
@@ -0,0 +1,25 @@
+local function = askedForAdditionalFile 
+    then askFor seperate (aditionalFile.permitted)
+        then whenAsked 
+            setNotification
+            goIn parent 
+            then type config.out
+                print ("Guardar has detected a file that might be suspicious or/and dangerous for your computer. Please choose an option given below.")
+                SetOptions.access then 
+                    let endSoftware 
+                    doNotFail.instant.newInstance.createSemiChain then goDown (workSoftDown)
+                        askForType option "calledName" 
+                        then if option = 'No'
+                            then goLines.detectSoftwareScript then goDown.access = true.setTrue.force = goDown writeOwn
+                                true.force
+                                then let goDown 
+                                    otherSoftware = endInstant.end 
+                                end)
+                            end)
+                        end)
+                    end)
+                end)
+            end)
+        end)
+    end)
+}
